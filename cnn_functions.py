bfc51152fd13f72ad460ca85193aeedfb28f470d
ar1050 <anrice@davidson.edu>
2021-06-11 16:58:52 -0400

    adding annabel's code

--- cnn_functions.py
+++ cnn_functions.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
-import annabel as anna
+import annabel_data_creation as anna
 import cnn_functions as cnn
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
@@ -32,4 +32,29 @@ def plot_learning_curve(history):
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.xticks(range(0, len(history.history['loss'])), range(1, len(history.history['loss']) + 1))
+    plt.show()
+    
+def plot_confusion_matrix(y_true, y_pred, classes, title=None, cmap=plt.cm.Blues):
+    if not title:
+        title = 'Confusion matrix'
+
+    cm = confusion_matrix(y_true, y_pred)
+    fig, ax = plt.subplots(figsize=(4, 4), dpi=100)
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+    plt.setp(ax.get_xticklabels(), rotation=45, ha='right', rotation_mode='anchor')
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], 'd'),
+                    ha='center', va='center',
+                    color='white' if cm[i, j] > thresh else 'black')
+    fig.tight_layout()
     plt.show()
